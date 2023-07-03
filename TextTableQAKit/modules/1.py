ad7ed8eec2c42888d406c597d510b9bb7ab4d921
lfy79001 <843265183@qq.com>
2023-07-03 01:32:01 +0000

    update

---
+++ TextTableQAKit/modules/1.py
@@ -0,0 +1,3 @@
+from datasets import load_dataset
+
+dataset = load_dataset("hybrid_qa")
--- TextTableQAKit/modules/TDataset.py
+++ TextTableQAKit/modules/TDataset.py
@@ -1,19 +1,25 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
+from utils import *
+from transformers import AutoTokenizer
+from tqdm import tqdm
 
-        
-        
+
+# question_id, question, answer,   
 class EncycDataset(Dataset):
-    def __init__(self, **kwargs):
-        import pdb; pdb.set_trace()
-        dataset_name = None
-        if 'dataset_name' in kwargs.keys():
-            dataset_name = kwargs['dataset_name']
-        if dataset_name != None:
+    def __init__(self, all_data, **kwargs):
+        super(EncycDataset, self).__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(kwargs['plm'])
+        self.data = []
+        for item in tqdm(all_data):
+            import pdb; pdb.set_trace()
+            data_i = {}
+            data_i
+            
             
         
-    
+        
     def __len__(self):
         pass
         
@@ -33,5 +39,43 @@ class SpreadsheetDataset(Dataset):
     def __getitem__(self, index):
         return feature, label
     
+    
+    
+class DatasetManager:
+    def __init__(self, **kwargs):
+        if kwargs['type'] == 'common': # 使用标准的数据集
+            # get the path of the dataset
+            dataset_path = dataset_download(kwargs['dataset_name'])
+            # load data  -> dict {'train', 'dev', 'test'}
+            dataset_data = load_data(dataset_path)
+            if kwargs['table_type'] == 'encyc':
+                train_dataset = EncycDataset(dataset_data['train'], **kwargs)
+                dev_dataset = EncycDataset(dataset_data['dev'], kwargs)
+                test_dataset = EncycDataset(dataset_data['test'], kwargs)
+                pass
+            elif kwargs['table_type'] == 'spreadsheet':
+                pass
+            elif kwargs['table_type'] == 'structured':
+                pass
+    def train():
+        pass
+    
 if __name__ == '__main__':
-    dataset = EncycDataset(dataset_name='hybridqa')
+    kwargs = {}
+    kwargs['type'] = 'common'
+    kwargs['table_type'] = 'encyc'
+    kwargs['dataset_name'] = 'hybridqa'
+    kwargs['plm'] = 'bert-base-uncased'
+    kwargs['dataset_config'] = {'id': 'question_id', 'question': 'question', 'answer': 'answer-text'}
+    
+    # need to write a function map the table to row
+    
+    
+    def aa():
+        print(11)
+        
+    aa()
+    
+    
+    datasetManager = DatasetManager(**kwargs)
+    # dataset = EncycDataset(dataset_name='hybridqa')
