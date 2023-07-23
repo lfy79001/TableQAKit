abc05a415c80818f9e5ebbe4cf4198b76e7b7d41
huajihuajiguai <18979195877@163.com>
2023-07-23 22:55:24 +0800

    leave upload and download interface

--- TextTableQAKit/static/js/custom.js
+++ TextTableQAKit/static/js/custom.js
@@ -1,3 +1,6 @@
+var table_html = '';
+var properties_html = '';
+var table_list = [];
 var model = 'T5-small';
 var host = '210.75.240.136';
 var port = '18889';
@@ -27,6 +30,80 @@ function selectFile() {
     $('#file-selector').click();
 }
 
+function changeTableListHtml() {
+    var table_list_html = '<tr><th><div style="width:100px;">Table Name</div></th><th><div>Action</div></th></tr>'
+    for (var index = 0; index < table_list.length; index) {
+        table_list_html += '<tr><td><div style="width:100px;"><b>' + table_list[index] + '</b></div></td><td><div class="row"><div class="col-md-8"><button type="button" class="btn btn-success " style="width:200px" onclick="selectTable(' + index + ')">Use This Table</button></div><div class="col-md-4"><button type="button" class="btn btn-danger " onclick="deleteTable(' + index + ')">Delete</button></div></div></td></tr>'
+    }
+    $('#table-list').html(table_list_html);
+}
+
+function changeTableHtml() {
+    $('#table-container').html(table_html);
+}
+
+function changePropertiesHtml() {
+    $('#properties-container').html(properties_html);
+}
+
+function selectTable(index) {
+    dataJson = { 'table_name': table_list[index] };
+
+    $.ajax({
+        type: 'POST',
+        url: url + '/custom/table',
+        chche: false,
+        async: true,
+        dataType: "json",
+        contentType: 'application/json',
+        data: JSON.stringify(dataJson),
+        success: (data) => {
+            if (data[success] == 'success') {
+                table_html = data.table_html;
+                changeTableHtml();
+                properties_html = data.properties_html;
+                changePropertiesHtml();
+            } else {
+                alert('fetch table failed!');
+            }
+        },
+        error: (XMLHttpRequest, textStatus, errorThrown) => {
+            alert('fetch table error!');
+            console.log(XMLHttpRequest.status);
+            console.log(XMLHttpRequest.readyState);
+            console.log(textStatus);
+        }
+    });
+}
+
+function deleteTable(index) {
+    dataJson = { 'table_name': table_list[index] };
+
+    $.ajax({
+        type: 'POST',
+        url: url + '/custom/remove',
+        chche: false,
+        async: true,
+        dataType: "json",
+        contentType: 'application/json',
+        data: JSON.stringify(dataJson),
+        success: (data) => {
+            if (data[success] == 'success') {
+                table_list.splice(index);
+                changeTableListHtml();
+            } else {
+                alert('table remove failed!');
+            }
+        },
+        error: (XMLHttpRequest, textStatus, errorThrown) => {
+            alert('fetch table error!');
+            console.log(XMLHttpRequest.status);
+            console.log(XMLHttpRequest.readyState);
+            console.log(textStatus);
+        }
+    });
+}
+
 function submitExcelFile() {
     var files = $('#table-file-input')[0].files[0]
     if (files.length == 0) {
@@ -41,8 +118,34 @@ function submitExcelFile() {
         data: fd,
         processData: false,
         contentType: false,
+        async: true,
         success: function(res) {
             console.log(res);
+        }
+    });
+}
+
+function getTable() {
+    var dataJson = { 'table_name': tablename };
+    $.ajax({
+        type: 'POST',
+        url: url + '/custom/table',
+        chche: false,
+        async: false,
+        dataType: "json",
+        contentType: 'application/json',
+        data: JSON.stringify(dataJson),
+        success: (data) => {
+            properties_html = data.properties_html;
+            table_html = data.table_html;
+            changeTableHtml();
+            changePropertiesHtml();
+        },
+        error: (XMLHttpRequest, textStatus, errorThrown) => {
+            alert('fetch table error!');
+            console.log(XMLHttpRequest.status);
+            console.log(XMLHttpRequest.readyState);
+            console.log(textStatus);
         }
     });
 }
@@ -68,17 +171,26 @@ function downloadExcel() {
     });
 }
 
+function downloadExcelExample() {
+    window.location.href = url + '/custom/example';
+}
+
+function init() {
+    changeTableListHtml();
+}
+
 $(document).ready(() => {
     url = 'http://' + host + ':' + port;
-    $('#switchToIndex').click(function() {
+    init();
+    $('#switchToIndex').click(() => {
         window.location.href = 'index.html';
     });
     $('#get-question-button').click(getAnswer);
     $('#upload-table').click(selectFile);
     $('#submit-excel-button').click(submitExcelFile);
     $('#select-excel-button').click(() => {
-        $('#table-file-input').click()
+        $('#table-file-input').click();
     });
     $('#table-file-input').change(changeSelectedExcelFile);
-    $('down-excel').change(downloadExcel);
+    $('#down-excel').change(downloadExcel);
 });
