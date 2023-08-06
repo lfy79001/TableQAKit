8d23a72512640a8cb060b9e2a508df3cbd03ffe0
yangpengqi <1002991997@qq.com>
2023-08-06 15:19:08 +0000

    add some ui

--- TableQAKit/gunicorn_config.py
+++ TableQAKit/gunicorn_config.py
@@ -2,7 +2,7 @@ import os
 
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
-workers = 1
+workers = 2
 bind = "210.75.240.136:18888"
 accesslog = "gunicorn_access.log"
 errorlog = "gunicorn_error.log"
