b5869165cce5ec9ecc83c49f8a657f022c567a61
shwj114514 <3278668047@qq.com>
2023-08-09 19:56:18 +0800

    fix strucuredqa import errors

--- ceshi.py
+++
@@ -1,6 +0,0 @@
-from TableQAKit.structuredqa import (
-    ToolBaseForStructuredQA,
-    BartForStructuredQA,
-    BertForStructuredQA)
-from TableQAKit.structuredqa.utils.dataset import WikisqlDataset, WikitablequestionsDataset, MSR_SQADataset, \
-    StructuredQADatasetFromBuilder_Tapas, StructuredQADatasetFromBuilder
